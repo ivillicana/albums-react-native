6b28a55298d1dd7fa168e1addf1be6875b5d4f86
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-06 18:55:21 -0400

    add styling

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 /* eslint-disable arrow-body-style */
 
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
   return (
-    <Header headerText={'Albums'} />
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
   );
 };
 
