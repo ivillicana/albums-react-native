38a5e1f8540c50beec95701679da79cae54b95a8
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-05 20:55:42 -0400

    create albjm detail component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,13 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumDetail = (props) => {
+  return (
+    <View>
+      <Text>{props.album.title}</Text>
+    </View>
+  );
+};
+
+export default AlbumDetail;
