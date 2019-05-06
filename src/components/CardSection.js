a17adc1a5df583064849488d87500ac8d185d62e
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-05 21:31:00 -0400

    change styling

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 
 const CardSection = (props) => {
   return (
-    <View stule={styles.containerStyle}>
+    <View style={styles.containerStyle}>
       {props.children}
     </View>
   );
